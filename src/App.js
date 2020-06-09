03bbed87eb83789bb2cb365b2d4b74b41a50c0d6
陳德元 <r06521608@ntu.edu.tw>
2020-06-09 12:55:08 -0500

    built the sign up component and custom buttom and input field

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
 import Header from './components/header/header.component.jsx';
-
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 
 function App() {
   return (
@@ -14,7 +14,8 @@ function App() {
     	<Header />
     	<Switch>
     		<Route exact path='/' component={HomePage} />
-    		<Route exact path='/shop' component={ShopPage} />
+    		<Route path='/shop' component={ShopPage} />
+    		<Route path='/signin' component={SignInAndSignUpPage} />
     	</Switch>
     </div>
   );
