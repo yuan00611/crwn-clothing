c7d01f335c2367258b6dda8e9186e248e0105d4a
陳德元 <r06521608@ntu.edu.tw>
2020-06-12 14:53:44 -0500

    build the checkout page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { createStructuredSelector } from 'reselect';
 
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component.jsx';
+import ShopPage from './pages/shop/shop.component';
+import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component.jsx';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
 import { setCurrentUser } from './redux/user/user.actions';
@@ -47,6 +48,7 @@ class App extends React.Component {
 	    	<Switch>
 	    		<Route exact path='/' component={HomePage} />
 	    		<Route path='/shop' component={ShopPage} />
+	    		<Route exact path='/checkout' component={CheckoutPage} />
 	    		<Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
 	    	</Switch>
 	    </div>
